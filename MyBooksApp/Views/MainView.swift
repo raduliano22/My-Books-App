d0882a9008fe078531fb5b1a8e7c297c0d139000
raduliano <radulian@me.com>
2022-04-11 15:05:41 +0300

    -MVVM pattern -add image assets -add json local file

---
+++ MyBooksApp/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyBooksApp
+//
+//  Created by Octav Radulian on 11.04.2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
