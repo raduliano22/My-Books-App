613eab69ca867772e3522e2251a2be7dd25a48d4
raduliano <radulian@me.com>
2022-04-11 15:00:18 +0300

    Initial Commit

---
+++ MyBooksApp/MyBooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksAppApp.swift
+//  MyBooksApp
+//
+//  Created by Octav Radulian on 11.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
